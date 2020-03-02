1960c90267047f6334801b8fb1ce3d3e7901449f
Attila Kovacs <ko.attila95@gmail.com>
2020-03-02 11:22:58 +0100

    5. - 6.- 7. exercise

--- 2.R
+++ 2.R
@@ -9,10 +9,21 @@ str(features)
 nrow(features[country %in% c("Hungary") & stars == 4,])
 
 # 3. Compute the average rating of 4 and 5 star hotels in Hungary and Germany
+a <- features[country %in% c("Hungary", "Germany") & stars %in% c(4,5),]
+mean(na.omit(a$rating))
+features[country %in% c("Hungary", "Germany") & stars %in% c(4,5),mean(rating, na.rm = TRUE)]
 # 4. Round up the previously computed average rating to 2 digits
 # 5. Do we have any bookings in unknown hotels (as per the features dataset)?
+bookings[!hotel_id %in% unique(features$hotel_id)]
+
 # 6. Clean up the bookings dataset from bookings from unknown hotels and print the number of remaining bookings
+bookings[hotel_id != 2]
+bookings[hotel_id %in% unique(features$hotel_id)]
+
 # 7. What's the average distance of hotels from the city central in Budapest?
+str(features)
+features[city == "Budapest" & accommodation_type == "Hotel", mean(distance, na.rm = TRUE)]
+
 # 8. List all neighbourhoods in Budapest
 # 9. Compute the average distance from the city center for the neighbourhoods in Budapest
 # 10. Count the number of bookings in Hungary
